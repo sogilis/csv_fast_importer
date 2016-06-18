21f87b5af700737059237de2cbc9ed8ee6366924
Jibidus <jibidus@gmail.com>
2016-06-18 21:05:37 +0200

    Chore / Move database.yml in config subfolder

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -5,7 +5,7 @@ require 'active_record'
 require 'pathname'
 
 test_dir = Pathname.new File.dirname(__FILE__)
-ActiveRecord::Base.configurations["test"] = YAML.load_file(test_dir.join("database.yml"))
+ActiveRecord::Base.configurations["test"] = YAML.load_file('spec/config/database.yml')
 ActiveRecord::Base.establish_connection :test
 
 %w(database_helper spec_helper).each { |file| require test_dir.join(file) }
